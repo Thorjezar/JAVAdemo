f41d37ebf5fbae4d51298bcbb7b4f9974d06847e
YWM <thorjezar@example.com>
2019-04-01 17:27:52 +0800

    20190401下午

---
+++ Test/src/Test.java
@@ -0,0 +1,22 @@
+public class Test {
+    public static void main(String[] args){
+/*        byte bytes = 3;
+        short s = 10;
+        int money = 1000;
+        long num = 300000;
+        float f = 3.5f;
+        double d = 3.5;
+        char ch = 'a';
+        char ch2 = '你';
+        boolean flag = true;
+        boolean flag2 = false;*/
+        String name = "小明";
+        int age = 25;
+        float year = 3.5f;
+        String hobby = "吃鸡";
+        System.out.println("学生信息");
+        System.out.println("姓名："+name);
+        System.out.println("工作了"+year);
+        System.out.println("兴趣爱好："+hobby);
+    }
+}
---
+++ helloworld/src/com/ywm/demo/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.ywm.demo;
+/*
+* 第一个
+* */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
